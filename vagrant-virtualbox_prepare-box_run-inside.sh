a222b16a5ceb000f4844e6f236a6296719cb5efa
Viktor Benei <viktor.benei@gmail.com>
2017-02-16 19:12:41 +0100

    Update vagrant-virtualbox_prepare-box_run-inside.sh

--- vagrant-virtualbox_prepare-box_run-inside.sh
+++ vagrant-virtualbox_prepare-box_run-inside.sh
@@ -10,8 +10,8 @@ set -ex
 
 VBOX_VER=5.1.14
 
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install linux-headers-$(uname -r) build-essential dkms
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms
 
 sudo sh -c 'echo >> /etc/sudoers'
 sudo sh -c 'echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
@@ -29,4 +29,3 @@ mkdir ~/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' >> ~/.ssh/authorized_keys
 
 chmod 0700 ~/.ssh && chmod 0600 ~/.ssh/authorized_keys
-sudo reboot
